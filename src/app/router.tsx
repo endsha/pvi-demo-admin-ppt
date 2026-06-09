1061da814cb8925e216f7b6c3e8854234da6b907
Alvin Nguyen <anh.ns.dev@gmail.com>
2026-06-09 14:40:50 +0700

    feat(account-settings): wire page to /cai-dat-tai-khoan route

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -6,6 +6,7 @@ import { PoliciesListPage } from '../pages/policies/PoliciesListPage'
 import { ReportsListPage } from '../pages/reports/ReportsListPage'
 import { InvoiceRequestsListPage } from '../pages/invoice-requests/InvoiceRequestsListPage'
 import { GsmSearchPage } from '../pages/gsm-search/GsmSearchPage'
+import { AccountSettingsPage } from '../pages/account-settings/AccountSettingsPage'
 
 export const router = createBrowserRouter([
   {
@@ -26,7 +27,7 @@ export const router = createBrowserRouter([
       { path: 'bao-cao-power-bi/quan-ly-bao-cao', element: <ReportsListPage /> },
       { path: 'bao-cao-power-bi/bao-cao-cong-khai', element: <PlaceholderPage title="Báo cáo công khai" /> },
       { path: 'bao-cao-power-bi/bao-cao-doanh-thu', element: <PlaceholderPage title="Báo cáo doanh thu" /> },
-      { path: 'cai-dat-tai-khoan', element: <PlaceholderPage title="Cài đặt tài khoản" /> },
+      { path: 'cai-dat-tai-khoan', element: <AccountSettingsPage /> },
     ],
   },
 ])
